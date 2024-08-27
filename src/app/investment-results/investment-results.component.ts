59daacbd87f11e81958c0dd10910d829e8e1485e
AlexDieterr <alexdieter1@gmail.com>
2024-08-26 19:07:49 -0700

    change to ngif and ngfor

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,21 +1,14 @@
-import { CurrencyPipe } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, computed, inject } from '@angular/core';
+
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [CurrencyPipe, CommonModule],
   templateUrl: './investment-results.component.html',
-  styleUrl: './investment-results.component.css'
+  styleUrl: './investment-results.component.css',
 })
 export class InvestmentResultsComponent {
-  @Input() results?: {
-    year: number,
-    interest: number,
-    valueEndOfYear: number,
-    annualInvestment: number,
-    totalInterest: number,
-    totalAmountInvested: number,
-  }[];
+  private investmentService = inject(InvestmentService);
+
+  results = computed(() => this.investmentService.resultData());
 }
